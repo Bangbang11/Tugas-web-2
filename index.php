963425989dcc1b4712b01a664571db3b77cdf0a8
bangbang11 <bangbanggrt@gmail.com>
2017-09-19 17:48:51 +0700

    tugas web, versi 1

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pendaftaran Siswa Baru | Tugas Web 2</title>
+</head>
+
+<body>
+	<header>
+		<h1 align="center">PENDAFTARAN SISWA BARU</h1>
+		<hr>
+		<br>
+		<br>
+		<h4 align="center">Tugas Mata Kuliah Pemrograman Web 2</h4>
+		<h4 align="center">Bangbang (12111010)</h4>
+	</header>
+
+	<h4 align="center">Menu</h4>
+	<nav>
+			<p align="center"> <a href="list-siswa.php">Data Siwa</a> </p>
+	</nav>
+
+	</body>
+</html>
